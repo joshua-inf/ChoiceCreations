ffb031cf39163645a0a4999afb24b2542646156a
joshua-inf <jsibanda407@gmail.com>
2024-11-14 11:43:37 +0200

    whatsapp link

--- src/Components/Whatwedoomp/Contacts.js
+++ src/Components/Whatwedoomp/Contacts.js
@@ -3,7 +3,9 @@ const Contacts = () => {
     return (
         <>
             <div style={{ position: 'fixed', zIndex: 3, bottom: "30px", right: "30px" }} className='btn-success btn rounded-3'>
-                <BiLogoWhatsapp size={30}/>
+                <a href="https://wa.link/ohb6ne" style={{color:'white'}}>
+                    <BiLogoWhatsapp size={30}/>
+                </a>
             </div>
         </>
     )
